f359ea9e499ed8a088fd7c76cf7632f1e80e7c64
PhatJack <tienphatng.693@gmail.com>
2023-11-24 20:24:23 +0700

    feat(font): customize new font

--- src/components/Sections/SectionOne.jsx
+++ src/components/Sections/SectionOne.jsx
@@ -3,9 +3,12 @@ import { SampleImg } from '../../assets'
 
 const SectionOne = () => {
 	return (
-		<div className='bg-img w-full h-screen '>
+		<div className='bg-img w-full h-screen relative'>
 			<img src={SampleImg} alt="" className='w-full h-full object-center' />
-			
+			<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2">
+				<p className='text-8xl font-'>Seamless <span>furniture</span>
+					with natural fabrics</p>
+			</div>
 		</div>
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
 	content: [
 		"./index.html",
@@ -7,17 +10,50 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				'white' : '#FCFDFD',
-				'black' : '#000000',
-				'stormy' : '#323334',
+				'white': '#FCFDFD',
+				'black': '#000000',
+				'stormy': '#323334',
 				'smoke': '#4E4F51',
-				'ash' : '#7C7F84',
-				'oyster' : '#DFDEDB',
-				'claudy' : '#F5F4F3',
-				'opal' : '#384353',
-				'linen' : '#F8F4E7'
+				'ash': '#7C7F84',
+				'oyster': '#DFDEDB',
+				'claudy': '#F5F4F3',
+				'opal': '#384353',
+				'linen': '#F8F4E7'
+			},
+			fontFamily: {
+				'montreal-neue': ['Montreal Neue', ...defaultTheme.fontFamily.sans],
+				'reckless-neue': ['Reckless Neue', ...defaultTheme.fontFamily.sans],
+				'reckless-neue-italic': ['Reckless Neue Italic', ...defaultTheme.fontFamily.sans]
 			}
 		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function ({ addBase }) {
+			addBase({
+				'@font-face': {
+					fontFamily: 'Montreal Neue',
+					fontWeight: '400',
+					src: 'url(/src/font/NeueMontreal-Regular.ttf)',
+				}
+			})
+		}),
+		plugin(function ({ addBase }) {
+			addBase({
+				'@font-face': {
+					fontFamily: 'Reckless Neue',
+					fontWeight: '400',
+					src: 'url(/src/font/RecklessNeue-Regular.ttf)',
+				}
+			})
+		}),
+		plugin(function ({ addBase }) {
+			addBase({
+				'@font-face': {
+					fontFamily: 'Reckless Neue Italic',
+					fontWeight: '400',
+					src: 'url(/src/font/RecklessNeue-RegularItalic.ttf)',
+				}
+			})
+		}),
+	],
 }
