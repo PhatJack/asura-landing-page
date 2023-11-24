4f4f2e8004ca2f9e232b37a2383106598e713c9d
PhatJack <tienphatng.693@gmail.com>
2023-11-24 09:24:03 +0700

    feat(tailwind): customize configuration2

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
 		"./src/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'white' : '#FCFDFD',
+				'black' : '#000000',
+				'stormy' : '#323334',
+				'smoke': '#4E4F51',
+				'ash' : '#7C7F84',
+				'oyster' : '#DFDEDB',
+				'claudy' : '#F5F4F3',
+				'opal' : '#384353',
+				'linen' : '#F8F4E7'
+			}
+		},
 	},
 	plugins: [],
 }
